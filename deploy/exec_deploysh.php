d325edc5fb3d66657d3b91260648bcc6ea93dc6a
masusugu <koide@intronworks.jp>
2015-05-23 16:42:36 +0900

    edited exec file

--- deploy/exec_deploysh.php
+++ deploy/exec_deploysh.php
@@ -1,5 +1,10 @@
 <?php 
-
-print_r($_SERVER);
-
+$path = '/path/to/deploy_shell.sh';
+if ($_POST['payload']) {
+	$decode = json_decode($_POST['payload']);
+		if ($decode->{'ref'} == 'refs/heads/develop') {
+			exec('sudo sh '.$path, $output);
+			print_r('deployed.');
+		}
+	}
 ?>
